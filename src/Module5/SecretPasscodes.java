fbae88420c7887678d082d2c85cf772e33453a61
dabaquita <dabaquita@gmail.com>
2017-09-19 16:31:12 -0400

    Additions to Module5 from 5.03 and 5.06 modules

---
+++ src/Module5/SecretPasscodes.java
@@ -0,0 +1,28 @@
+package Module5;
+
+/**
+ * This program generates random passwords,
+ * letting the user select character set
+ * and the length.
+ *
+ * @author Denielle Kirk Abaquita
+ * @version
+ *
+ */
+
+/**
+ * Planning / Pseudocode
+ *
+ * import Scanner
+ * import IOException
+ * import File
+ * import PrintWriter
+ *
+ * --- Main (throws IOException) ---
+ *
+ * --> Variables
+ *
+ */
+
+public class SecretPasscodes {
+}
